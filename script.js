0a1d9ce39cbc56eb3d1e81c4113b0ff51237d17c
TomTomGRUB <thomasnecionis@gmai.com>
2024-09-04 16:50:41 +0100

    Changed scroll function to be based off elements instead of scroll position + minor changes

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let projects = document.getElementById("projectsBtn");
 let about = document.getElementById("aboutBtn");
 let contact = document.getElementById("contactBtn");
 
+let homeElement = document.getElementsByTagName("header")[0];
+let projectsElement = document.getElementById("projectPage");
+let aboutElement = document.getElementById("aboutMe");
+let contactElement = document.getElementById("contact");
 
 home.addEventListener("click", scroll);
 projects.addEventListener("click", scroll);
@@ -10,36 +14,51 @@ about.addEventListener("click", scroll);
 contact.addEventListener("click", scroll);
 window.addEventListener("scroll", handleScroll);
 
-
-//*********change to be based off of elements rather than coords*****************
-
-
 function scroll(event){
-	let coord;
-	if(event.target.id === "homeBtn") coord = 0;
-	else if(event.target.id === "projectsBtn") coord = 1025;
-	else if(event.target.id === "aboutBtn") coord = 2250;
-	else coord = 3300;
+	const btnId = event.target.id;
+	let element = homeElement;
+	let offsetPosition = window.pageYOffset;
+
+	if(btnId === "homeBtn") {
+		element = homeElement;
+	}
+	else if(btnId === "projectsBtn") {
+		element = projectsElement;
+		offsetPosition -= 50;
+	}
+	else if(btnId === "aboutBtn") {
+		element = aboutElement;
+		offsetPosition -= 100;
+	}
+	else if(btnId === "contactBtn") {
+		element = contactElement;
+	}
+
+	const elementPosition = element.getBoundingClientRect().top;
+	offsetPosition += elementPosition;
 
 	//prevents button-triggered scroll from also triggering manual scroll handling function
 	window.removeEventListener("scroll", handleScroll);
 	setTimeout(() => { window.addEventListener("scroll", handleScroll); }, 500); 
 
-	window.scrollTo({top: coord});
+	window.scrollTo({top: offsetPosition});
 	highlightPageBtn(event.target);
 }
 
+
 function handleScroll(){
-	if(window.scrollY <= 200){
+	if(window.scrollY <= homeElement.getBoundingClientRect().top){
 		highlightPageBtn(home);
 	}
-	else if(window.scrollY > 400 && window.scrollY < 1500){
+	else if(window.scrollY > projectsElement.getBoundingClientRect().top && window.scrollY < projectsElement.getBoundingClientRect().bottom){
 		highlightPageBtn(projects);
 	}
-	else if(window.scrollY > 1500 && window.scrollY < 2500){
+	else if(window.scrollY > aboutElement.getBoundingClientRect().top && window.scrollY < aboutElement.getBoundingClientRect().bottom){
 		highlightPageBtn(about);
 	}
-	else if(window.scrollY > 2500) highlightPageBtn(contact);
+	else if(window.scrollY > contactElement.getBoundingClientRect().top){
+	 	highlightPageBtn(contact);
+	}
 }
 
 function highlightPageBtn(page){
